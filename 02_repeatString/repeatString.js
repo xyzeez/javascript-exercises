2998d75e7569200d386fe05d2a8db2063a27c638
xyzeez <106898953+xyzeez@users.noreply.github.com>
2023-07-17 02:51:24 +0100

    solved to 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num >= 0) {
+        let string = '';
+        for (let i = 0; i < num; i++) {
+            string += str;
+        }
+        return string;
+    } else return 'ERROR';
 };
 
 // Do not edit below this line
