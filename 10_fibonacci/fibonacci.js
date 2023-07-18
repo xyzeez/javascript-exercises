17bf8b326f10d01892cf8346c1b08b97b7269920
xyzeez <106898953+xyzeez@users.noreply.github.com>
2023-07-18 01:55:57 +0100

    Solved till 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num > 0) {
+        let fabArr = [1];
+        let start = 0;
+        let stop = fabArr[0];
+        for (let i = 1; i < num; i++) {
+            fabArr[i] = start + stop;
+            start = fabArr[i-1];
+            stop = fabArr[i];
+        }
+        return fabArr[num-1];
+    } else return 'OOPS';
 };
 
 // Do not edit below this line
