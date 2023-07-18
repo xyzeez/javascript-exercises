8533c23598e5b4750b43ec41cba7d9cc0c9192a8
xyzeez <106898953+xyzeez@users.noreply.github.com>
2023-07-18 13:23:47 +0100

    Finsihed exercises

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    const titleArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        titleArr.push(arr[i].title);
+    }
+    return titleArr;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(person) {
+    const ageArr = [];
+    for (let i = 0; i < person.length; i++) {
+        let age;
+        if (person[i].yearOfDeath) {
+            age = (person[i].yearOfDeath - person[i].yearOfBirth);
+        } else {
+            person[i].yearOfDeath = new Date().getFullYear();
+            age = (person[i].yearOfDeath - person[i].yearOfBirth);
+        }
+        ageArr.push(age);
+    }
+    let oldestAge = 0;
+    let oldestAgeIndex;
+    for (let i = 0; i < ageArr.length; i++) {
+        if (oldestAge < ageArr[i]) {
+            oldestAge = ageArr[i];
+            oldestAgeIndex = i;
+        }
+    }
+    return person[oldestAgeIndex];
 };
 
 // Do not edit below this line
